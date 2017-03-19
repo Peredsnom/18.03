37ce215b5a4f08df3073584e096d0a698c8438a5
Александр Дякив < >
2017-03-19 16:31:43 +0300

    Initial commit

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1529;
+
+/**
+ * Created by днс on 19.03.2017.
+ */
+public class Plane implements Flyable
+{
+    int passangers;
+    Plane(int passangers) {
+        this.passangers = passangers;
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
